43e974703b7b78e44439e2214bb6d046fad0d88f
maiyo008 <tonymaiyo008@gmail.com>
2023-01-24 10:21:02 +0000

    Requirement correction

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -25,3 +25,19 @@ void bubble_sort(int *array, size_t size)
 			return;
 	}
 }
+
+/*
+ * swap - Function to swap any two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: 0 when the execution is complete
+ */
+int swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	return (0);
+}
